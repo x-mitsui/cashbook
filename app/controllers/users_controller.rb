2560d812f4a203bc7a374f5309ad4d2d6d8ae440
dzq <x_mitsui@163.com>
2022-08-10 15:42:28 +0800

    添加controller层,路由,基本数据的增加\查看

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new email: "x_mitsui@163.com", name: "x_mitsui"
+    if user.save
+      p "save成功"
+      render json: user
+    else
+      p "save失败"
+      render json: user.errors
+    end
+  end
+
+  def show
+    # 尽量不直接用User.find，它找不到目标会直接返回预设的信息
+    # 如果想指定返回信息用find_by_id
+    usr = User.find_by_id params[:id]
+    if usr
+      render json: usr
+    else
+      head 404
+    end
+  end
+end
