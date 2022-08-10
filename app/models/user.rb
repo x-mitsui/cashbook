9a052ed01d7fa3783addd24bfc040593c71bdbe9
dzq <x_mitsui@163.com>
2022-08-10 18:55:00 +0800

    创建路由

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  # 分号紧挨email
   validates :email, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show"
+  namespace :api do
+    namespace :v1 do
+      # bin/rails routes 查看生成的路由
+      resources :validate_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags
+    end
+  end
 end
