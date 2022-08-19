f9dedaa5802b90ff9cec73fcbc5c82c46bfc3dfc
dzq <x_mitsui@163.com>
2022-08-19 17:11:38 +0800

    添加了一些注释；增加了一些换行

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -2,6 +2,7 @@ DB_PASSWORD=123456
 container_name=mangosteen-prod-1
 
 # version由1.1.1文件创建
+# 省去了数据库的创建，直接使用之前在本地创建的数据库
 version=$(cat mangosteen_deploy/version)
 
 echo 'docker build ...'
@@ -10,6 +11,13 @@ if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
 echo 'docker rm ...'
 docker rm -f $container_name
 fi
+
 echo 'docker run ...'
-docker run -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+docker run -d -p 3000:3000 \
+--name=$container_name \
+-e DB_HOST=$DB_HOST \
+-e RAILS_MASTER_KEY=$RAILS_MASTER_KEY \
+-e DB_PASSWORD=$DB_PASSWORD \
+--network=network1 \
+mangosteen:$version
 echo 'DONE!'
