b59f4616e0a0812c7994b86dc28a9a9102351dfa
dzq <x_mitsui@163.com>
2022-11-17 20:51:30 +0800

    按时间排序code，只拿最新的

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email)
-    validation_code = ValidationCode.find_by_email(email)
+    # desc降序排序，让其只拿最新的code
+    validation_code = ValidationCode.order(create_at: :desc).find_by_email(email)
     p validation_code.code
     @code = validation_code.code
     mail(to: email, subject: "山竹记账验证码")
