055ffebc8aeaf56f9410528c6f576e093dd6e3b6
dzq <x_mitsui@163.com>
2022-11-17 19:15:10 +0800

    优化文档+测试

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::ValidationCodesController < ApplicationController
                                          kind: "sign_in",
                                          code: code
     if validation_code.save
-      render json: { code: code }, status: 200
+      render status: 200
     else
       render json: { errors: validation_code.errors }, status: 400
     end
