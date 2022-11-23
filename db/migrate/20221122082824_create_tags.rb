c7438d3cae22a97cd9c9665bf035d4fdff1a170c
dzq <x_mitsui@163.com>
2022-11-23 16:07:25 +0800

    重置tag数据库表

--- db/migrate/20221122082824_create_tags.rb
+++ db/migrate/20221122082824_create_tags.rb
@@ -1,9 +1,9 @@
 class CreateTags < ActiveRecord::Migration[7.0]
   def change
     create_table :tags do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :name
-      t.string :sign
+      t.references :user, null: false, foreign_key: false
+      t.string :name, null: false
+      t.string :sign, null: false
       t.datetime :delete_at
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_025812) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_082824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,8 +26,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_025812) do
 
   create_table "tags", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "name"
-    t.string "sign"
+    t.string "name", null: false
+    t.string "sign", null: false
     t.datetime "delete_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -50,5 +50,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_025812) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "tags", "users"
 end
