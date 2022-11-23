4e30ce1909671af6fc7345984313497ce27a75bd
dzq <x_mitsui@163.com>
2022-11-23 17:57:54 +0800

    单个标签的获取和文档生成

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -10,6 +10,12 @@ class Api::V1::TagsController < ApplicationController
     } }
   end
 
+  def show
+    tag = Tag.find params[:id]
+    return head :forbidden unless tag.user_id == request.env["current_user_id"]
+    render json: { resource: tag }
+  end
+
   def create
     current_user = User.find request.env["current_user_id"]
     return render status: 401 if current_user.nil?
