d991c7b16d11c8b77c4767a1cb4597bc965e4660
dzq <x_mitsui@163.com>
2022-11-24 15:22:08 +0800

    优化部署脚本

--- bin/pack_for_host.sh
+++ bin/pack_for_host.sh
@@ -1,5 +1,4 @@
-# 注意修改 oh-my-env 目录名为你的目录名【从第7行能看出是工作目录】
-# 赋值两边是紧凑的，没有空格，书写时注意
+# 注意修改 oh-my-env 目录名为你的目录名
 dir=oh-my-env
 
 time=$(date +'%Y%m%d-%H%M%S')
@@ -19,26 +18,9 @@ function title {
   echo 
 }
 
-# 删除多余的文件
 yes | rm tmp/mangosteen-*.tar.gz; 
 yes | rm $deploy_dir/mangosteen-*.tar.gz; 
 
-# -c 或--create 建立新的备份文件。
-# -x或--extract或--get 从备份文件中还原文件。
-# -z或--gzip或--ungzip 通过gzip指令处理备份文件。
-# -v或--verbose 显示指令执行过程。
-# -f<备份文件>或--file=<备份文件> 指定备份文件。
-# 最后*代表所有不以“.”开头的文件
-# $dist为打包结果
-
-# tar --exclude="tmp/cache/*" --exclude="config/credentials/production.key" -czv -f $dist *
-# # p, --parents 需要时创建上层目录，如目录早已存在则不当作错误
-# # 将关键四个文件移动到两端共享的文件夹中
-# mkdir -p $deploy_dir
-# cp $current_dir/../config/host.Dockerfile $deploy_dir/Dockerfile
-# cp $current_dir/setup_host.sh $deploy_dir/
-# mv $dist $deploy_dir
-# echo $time > $deploy_dir/version
 title '打包源代码'
 tar --exclude="tmp/cache/*" --exclude="tmp/deploy_cache/*" --exclude="vendor/*" -cz -f $dist *
 title "打包本地依赖 ${vendor_1}"
--- bin/setup_remote.sh
+++ bin/setup_remote.sh
@@ -1,17 +1,18 @@
-user=dzq
+user=mangosteen
 root=/home/$user/deploys/$version
 container_name=mangosteen-prod-1
-# 运行数据库的容器名，也就是将来手动输入的DB_HOST
 db_container_name=db-for-mangosteen
 
 function set_env {
   name=$1
+  hint=$2
+  [[ ! -z "${!name}" ]] && return
   while [ -z "${!name}" ]; do
-    echo "> 请输入 $name:"
+    [[ ! -z "$hint" ]] && echo "> 请输入 $name: $hint" || echo "> 请输入 $name:" 
     read $name
-    sed -i "1s/^/export $name=${!name}\n/" ~/.bashrc
-    echo "${name} 已保存至 ~/.bashrc"
   done
+  sed -i "1s/^/export $name=${!name}\n/" ~/.bashrc
+  echo "${name} 已保存至 ~/.bashrc"
 }
 function title {
   echo 
@@ -24,33 +25,22 @@ function title {
 title '设置远程机器的环境变量'
 set_env DB_HOST
 set_env DB_PASSWORD
-set_env RAILS_MASTER_KEY
+set_env RAILS_MASTER_KEY '请将 config/credentials/production.key 的内容复制到这里'
 
-title '网络操作'
-if [ ! "$(docker network ls | grep network1)" ]; then
-  echo "创建network1 ..."
-  docker network create network1
-else
-  echo "网络network1已经存在"
-fi
-echo '网络操作完毕！'
-
-title '数据库操作'
+title '创建数据库'
 if [ "$(docker ps -aq -f name=^${DB_HOST}$)" ]; then
   echo '已存在数据库'
 else
-  echo '创建数据库'
-  docker run -d --name $db_container_name \
+  docker run -d --name $DB_HOST \
             --network=network1 \
-            -e POSTGRES_DB=mangosteen_production \
             -e POSTGRES_USER=mangosteen \
+            -e POSTGRES_DB=mangosteen_production \
             -e POSTGRES_PASSWORD=$DB_PASSWORD \
             -e PGDATA=/var/lib/postgresql/data/pgdata \
             -v mangosteen-data:/var/lib/postgresql/data \
             postgres:14
   echo '创建成功'
 fi
-echo '数据库操作完毕！'
 
 title 'docker build'
 docker build $root -t mangosteen:$version
@@ -78,8 +68,4 @@ case $ans in
     ""     )  echo "no" ;;
 esac
 
-title '删除历史tar等文件'
-shopt -s extglob
-cd /home/$user/deploys
-rm -vrf !($version)
 title '全部执行完毕'
