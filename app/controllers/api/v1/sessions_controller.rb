9d0a72fa670265f011cb570d91c3206cdad6d230
dzq <x_mitsui@163.com>
2022-12-02 16:00:41 +0800

    融入ActiveModel session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,14 +2,12 @@ require "jwt"
 
 class Api::V1::SessionsController < ApplicationController
   def create
-    if Rails.env.test?
-      return render status: :unauthorized if params[:code] != "123456"
+    session = Session.new params.permit :email, :code
+    if session.valid?
+      user = User.find_or_create_by email: session.email
+      render json: { jwt: user.generate_jwt }
     else
-      canSignin = ValidationCode.exists? email: params[:email], code: params[:code], used_at: nil
-      return render status: :unauthorized unless canSignin
+      render json: { errors: session.errors }, status: :unprocessable_entity
     end
-    # 用户不存在就创建一个，方便了测试，实际我觉得这样做是不符合业务逻辑的
-    user = User.find_or_create_by email: params[:email]
-    render status: :ok, json: { jwt: user.generate_jwt }
   end
 end
