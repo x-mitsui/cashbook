0813c904632acc9fdb101e3d6e3422f8171a3f02
dzq <x_mitsui@163.com>
2022-12-10 23:59:32 +0800

    工厂中直接创建和user有关联的id

--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -3,8 +3,8 @@ require "faker"
 FactoryBot.define do
   factory :item do
     user
-    amount { Faker::Number.number(digits: 4) } # 生成4位数数字
-    tag_ids { [Faker::Number.number(digits: 4)] } # 默认使用随机数字，等外面使用时覆盖个和user有关联的tag_id
+    amount { Faker::Number.number(digits: 4) }
+    tag_ids { [(create(:tag, user: user)).id] }
     happened_at { Faker::Date.between(from: 2.days.ago, to: Date.today) }
     kind { "expenses" }
   end
